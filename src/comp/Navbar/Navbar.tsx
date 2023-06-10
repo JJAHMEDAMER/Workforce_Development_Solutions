85666b988c6130601ec4b92dbfd5041bf6f1e0d8
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-06-10 21:54:24 +0300

    add search box to navbar

--- src/comp/Navbar/Navbar.tsx
+++ src/comp/Navbar/Navbar.tsx
@@ -7,6 +7,14 @@ export const Navbar = () => {
         <img className={style.logo_img} src="./students_inc_logo.svg" alt="students_inc_logo" />
         <p className={style.logo_text}><span>Students</span> Inc.</p>
       </a>
+
+      <label htmlFor="search" className={style.search_container}>
+        <input className={style.search}
+          placeholder="Search..." type="text" name="search" id="search" />
+        <div className={style.search_icon}>
+          <img src="./search.svg" alt="search" />
+        </div>
+      </label>
     </nav>
   )
 }
