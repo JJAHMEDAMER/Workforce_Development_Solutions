2f701c477b512015c3f4414f17c7b93520c2cef8
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-06-10 21:53:52 +0300

    add logo to nav bar

--- src/comp/Navbar/Navbar.tsx
+++ src/comp/Navbar/Navbar.tsx
@@ -1,6 +1,12 @@
+import style from "./Navbar.module.css"
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className={style.nav}>
+      <a className={style.logo} href="#">
+        <img className={style.logo_img} src="./students_inc_logo.svg" alt="students_inc_logo" />
+        <p className={style.logo_text}><span>Students</span> Inc.</p>
+      </a>
+    </nav>
   )
 }
